d8925f5869d7fb99f3dbe6b62b00335fc8acb8ba
Emily <66390405+rushingwater13@users.noreply.github.com>
2026-02-18 16:36:49 +0000

    Add reshaping of data

--- model.py
+++ model.py
@@ -1,11 +1,30 @@
-import tensorflow as tf
+#import tensorflow as tf
 import pandas as pd
 import numpy as np
+from sklearn.utils import shuffle
 
-data = pd.read_csv("Gaussian 2D Wide.csv")
+# Read in the data
+data = pd.read_csv("Gaussian 2D Wide.csv", header=None)
 data = data.values
 
-class1 = data[:, 0:2]
-class2 = data[:, 2:4]
+# Divide the classes
+class0 = data[:, 0:2]
+class1 = data[:, 2:4]
 
-stack = np.vstack((class1, class2))
+# Combine the classes into one array
+X = np.vstack((class0, class1))
+
+# Create an array to hold the classifications
+y_class0 = np.zeros(len(class0))
+y_class1 = np.ones(len(class1))
+
+# Add the classifications to the data array
+y = np.concatenate((y_class0, y_class1))
+
+# Shuffle to distribute the data
+X, y = shuffle(X, y, random_state=42)
+
+print(len(X))
+print(len(y))
+print(X.shape)
+print(y.shape)
