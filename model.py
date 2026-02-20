584bd15ccbb8594456f241673cd1369f4d6d0c5c
Emily <66390405+rushingwater13@users.noreply.github.com>
2026-02-20 16:02:20 +0000

    Add working tensorflow model

--- model.py
+++ model.py
@@ -1,9 +1,13 @@
-#import tensorflow as tf
 import pandas as pd
 import numpy as np
 from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import keras
+from keras import layers
+from keras import models
 
-# Read in the data
+# Read in the data, don't forget to capture the first row
 data = pd.read_csv("Gaussian 2D Wide.csv", header=None)
 data = data.values
 
@@ -28,3 +32,66 @@ print(len(X))
 print(len(y))
 print(X.shape)
 print(y.shape)
+
+
+
+# Divide into train + validate and test
+X_temp, X_test, y_temp, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
+
+# Now divide into train and validate
+X_train, X_val, y_train, y_val = train_test_split(
+    X_temp, y_temp,
+    test_size=0.25,
+    random_state=42,
+    stratify=y_temp
+)
+
+
+
+# Set up the model, using tensorflow keras
+# Sequential because Feed Forward
+# ReLU in the hidden layer because the data set is needs curvature
+# Sigmoid for the output layer because this is binary classification
+model = models.Sequential([
+    layers.Dense(units=16, activation='relu', input_shape=(2,)),
+    layers.Dense(1, activation='sigmoid')
+])
+
+
+# Compile the model
+# Adam optimizer for gradient descent, seems liek the default
+# Binary Crossentropy because binary classification
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+
+# Train the model
+model.fit(X_train, 
+    y_train, 
+    epochs=100, 
+    validation_data=(X_val, y_val)
+)
+
+
+# Evaluate the model on the test data
+results = model.evaluate(X_test, y_test)
+
+print(results)
+
+# what kind of data do we need to report?
+# so confusion matrix
+
+
+
+# https://www.tensorflow.org/tutorials/quickstart/beginner
+# https://www.freecodecamp.org/news/binary-classification-made-simple-with-tensorflow/
+# https://keras.io/guides/training_with_built_in_methods/
